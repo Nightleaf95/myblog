9fdadceacb9d0c292e72c65bf1b0ec47b7fe60a6
kevinyee1995 <649779829@qq.com>
2019-11-06 15:37:39 +0800

    11-06 version2.1 add Admin

---
+++ application/views/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--    项目的基准路径 (目前为：127.0.0.1/myblog/index.php)-->
+    <base href="<?php echo site_url(); ?>">
+    <title>YeKaiQi's blog</title>
+</head>
+<body>
+    <h1>博客主页</h1>
+</body>
+</html>
